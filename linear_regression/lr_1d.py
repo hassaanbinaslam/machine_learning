0db1fd5d3f0e9b534b871ccb6a1f569c737f7766
Aslam, Hassaan Bin <hassaanbinaslam@gatech.edu>
2019-04-14 15:59:37 +0500

    r_squared calculated

--- linear_regression/lr_1d.py
+++ linear_regression/lr_1d.py
@@ -30,9 +30,25 @@ b = (np.mean(Y)*np.mean(X**2) - np.mean(X) * np.mean(X*Y)) / denominator
 
 #predict y = Yhat
 Yhat = a*X + b
-print(Yhat)
 
 #plot Yhat
 plt.scatter(X, Y)
 plt.plot(X, Yhat)
 plt.show()
+
+#calculate r-squared
+SS_res = np.square(Y-Yhat).sum()
+SS_total = np.square(Y-Y.mean()).sum()
+R2 = 1 - SS_res/SS_total
+print("r-squared is: ", R2)
+
+#alternate method -> lazy_programmer
+d1 = Y-Yhat
+d2 = Y - Y.mean()
+r2 = 1 - d1.dot(d1) / d2.dot(d2)
+print("r-squared is: ", r2)
+
+#Note: dot product is same as sum of suqared numbers
+r = np.array([1,2,3,4])
+print("a.dot(a): ", r.dot(r))
+print("(a**2).sum: ", (r**2).sum())
--- linear_regression/notes_lr_1d.md
+++ linear_regression/notes_lr_1d.md
@@ -1,4 +1,7 @@
+
 # Notes for lr_1d.py
+
 ![Note 1](Notes/1.jpg)
 ![Note 2](Notes/2.jpg)
 ![Note 3](Notes/3.jpg)
+![Note 4](Notes/4.jpg)
