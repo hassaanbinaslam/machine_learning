905f9483bde1e97e33db343a6a5a21ae85e77e55
Aslam, Hassaan Bin <hassaanbinaslam@gatech.edu>
2019-04-24 13:46:15 +0500

    feature scaling
    feature scaling

--- data_preprocessing/data_processing.py
+++ data_preprocessing/data_processing.py
@@ -34,3 +34,10 @@ X = onehotencoder.fit_transform(X).toarray()
 # cross validation: split the data into train and test data
 from sklearn.model_selection import train_test_split 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
